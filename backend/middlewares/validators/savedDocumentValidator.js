fad96b11aeb46316c74acd158de86a17d88b4d18
Van Dao <doanvandao2411@gmail.com>
2025-06-12 10:15:18 +0700

    Deploy frontend to GitHub Pages

--- backend/middlewares/validators/savedDocumentValidator.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator');
-
-exports.saveDocumentRules = [
-    body('documentId', 'Vui lòng cung cấp ID tài liệu.')
-        .notEmpty(),
-    
-    body('documentId', 'ID tài liệu không hợp lệ.')
-        .isMongoId()
-];
